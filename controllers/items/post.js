a27f18e63854aba663a7a9a05bf892ab41d009d4
Leon Si <leonzalion@gmail.com>
2020-03-30 17:40:42 -0400

    hi

--- controllers/items/post.js
+++ controllers/items/post.js
@@ -2,7 +2,6 @@ const GroceryItem = require('../../models/GroceryItem');
 const to = require('await-to-js').default;
 
 module.exports = async (req, res) => {
-  console.log(req.body);
   const [createErr, createRes] = await to(GroceryItem.create(req.body));
   if (createErr) return res.status(400).json(createErr);
   return res.json(createRes);
