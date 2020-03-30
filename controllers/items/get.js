971974f2dd03e859b43c3839315b472f4f0c42da
Leon Si <leonzalion@gmail.com>
2020-03-30 18:52:11 -0400

    hi

--- controllers/items/get.js
+++ controllers/items/get.js
@@ -1,7 +1,10 @@
 const GroceryItem = require('../../models/GroceryItem');
+const to = require('await-to-js');
 
 module.exports = async (req, res) => {
-  GroceryItem.find({
+  const [findErr, findRes] = await to(GroceryItem.find({
     name: {$regex: req.query.filter || ''}
-  }).then(result => res.json(result));
+  }).exec());
+  if (findErr) return res.status(400).json(findErr);
+  return res.json(findRes);
 };
--- controllers/items/post.js
+++ controllers/items/post.js
@@ -2,7 +2,10 @@ const GroceryItem = require('../../models/GroceryItem');
 const to = require('await-to-js').default;
 
 module.exports = async (req, res) => {
-  const [createErr, createRes] = await to(GroceryItem.create(req.body));
+  const {name, rating, price, imageUrl, link} = req.body;
+  const [createErr, createRes] = await to(GroceryItem.create({
+    name, rating, price, imageUrl, link
+  }));
   if (createErr) return res.status(400).json(createErr);
   return res.json(createRes);
 };
