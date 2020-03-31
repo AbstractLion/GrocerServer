2346169c4648a429ac7cee2a0323563d74f0016b
Leon Si <leonzalion@gmail.com>
2020-03-31 00:14:51 -0400

    a noob

--- controllers/items/get.js
+++ controllers/items/get.js
@@ -4,7 +4,7 @@ const to = require('await-to-js').default;
 module.exports = async (req, res) => {
   const [findErr, findRes] = await to(GroceryItem.find({
     name: {$regex: req.query.filter || ''}
-  }, null, {skip: req.query.skip, limit: req.query.first}).exec());
+  }, null, {skip: +req.query.skip, limit: +req.query.first}).exec());
   if (findErr) return res.status(400).json(findErr);
   return res.json(findRes);
 };
