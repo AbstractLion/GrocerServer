c15e4a707034bcdf1f5a81b631a947b4c48f9cba
Bullmeza <56707028+Bullmeza@users.noreply.github.com>
2020-04-01 23:01:22 -0400

    Update activate.js

--- controllers/lists/activate.js
+++ controllers/lists/activate.js
@@ -1,12 +1,19 @@
-const GroceryList = require('../../models/GroceryList');
+const User = require('../../models/User');
+const User = require('../../models/GroceryList');
 const to = require('await-to-js').default;
 
-module.exports = async (req, res) => {
-  const {qrCode} = req.params;
-  const [findErr, findRes] = await to(GroceryList.findOne({qrCode}).exec());
-  if (findErr) return res.status(400).json(findErr);
-  if (findRes.activated) return res.status(301).json({message: "Already activated."});
-  findRes.activated = true;
-  await findRes.save();
-  return res.json(findRes);
+module.exports = async(req, res) => {
+    const { userId, qrCode } = req.body;
+    const [findErr, groceryObject] = await to(GroceryList.findOne(qrCode).exec());
+    if (findErr) return res.status(400).json(findErr);
+    let activated = groceryObject.activated;
+    if (activated) {
+        return res.json({ message: "Already Accepted" });
+    }
+    groceryObject.activated = true;
+    const [updateErr] = await to(User.findOneAndUpdate({ _id: userId }, { activatedGroceryList: groceryObject._id }).exec());
+    if (updateErr) return res.status(400).json(updateErr);
+    groceryObject.shopper = userId;
+    await groceryObject.save();
+    return res.json({ message: "Accepted" });
 };
