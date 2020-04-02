a132052147235d97e85d15ea1c1c38f9069079ac
Bullmeza <56707028+Bullmeza@users.noreply.github.com>
2020-04-01 21:04:27 -0400

    Update post.js

--- controllers/lists/post.js
+++ controllers/lists/post.js
@@ -6,16 +6,11 @@ module.exports = async (req, res) => {
   const {author, qrCode, items, createdAt} = req.body;
   let itemsArr = [];
   for (let [key, item] of Object.entries(items)){
-    const {title, price, imageUrl, rating} = item;
-    const [itemErr, itemRes] = await to(GroceryItem.create({
-      title, price, imageUrl, rating
-    }).exec());
-    if (itemErr) return res.status(400).json(itemErr);
-    itemsArr.push(itemRes._id);
+    itemsArr.push(key);
   }
   console.log(itemsArr);
   const [createErr, createRes] = await to(GroceryList.create({
-    author, qrCode, itemsArr, createdAt
+    author, qrCode, items: itemsArr, createdAt
   }));
   if (createErr) return res.status(400).json(createErr);
   return res.json(createRes);
