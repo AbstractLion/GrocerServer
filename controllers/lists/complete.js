51e76337448551d7c196579e701a92b5ac24e2d6
Bullmeza <56707028+Bullmeza@users.noreply.github.com>
2020-04-01 22:27:32 -0400

    Update complete.js

--- controllers/lists/complete.js
+++ controllers/lists/complete.js
@@ -5,7 +5,7 @@ const to = require('await-to-js').default;
 let expo = new Expo();
 
 module.exports = async(req, res) => {
-    const [findErr, findRes] = await to(GroceryList.findOne({ qrCode: req.params.id }).populate('user').populate('store').exec());
+    const [findErr, findRes] = await to(GroceryList.findOne({ qrCode: req.params.qrCode }).populate('user').populate('store').exec());
     if (findErr) return res.status(400).json(findErr);
     let user = findRes.user;
     let store = findRes.store;
