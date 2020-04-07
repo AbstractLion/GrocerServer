c10be7d1a728e77dac98bf0a531718412694404b
Leon Si <leonzalion@gmail.com>
2020-04-07 09:31:47 -0400

    made changes

--- controllers/lists/post.js
+++ controllers/lists/post.js
@@ -2,13 +2,13 @@ const GroceryList = require('../../models/GroceryList');
 const to = require('await-to-js').default;
 
 module.exports = async (req, res) => {
-  const {author, qrCode, items, createdAt} = req.body;
+  const {user, qrCode, items, createdAt} = req.body;
   let itemsArr = [];
   for (let [key, item] of Object.entries(items)) {
     itemsArr.push({id: key, count: item.count});
   }
   const [createErr, createRes] = await to(GroceryList.create({
-    author, qrCode, items: itemsArr, createdAt
+    user, qrCode, items: itemsArr, createdAt
   }));
   if (createErr) return res.status(400).json(createErr);
   return res.json(createRes);
