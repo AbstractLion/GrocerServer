0a2458d0d7d3462228ab4b820429f34459a8541a
Leon Si <leonzalion@gmail.com>
2020-04-07 18:45:45 -0400

    made changes

--- controllers/lists/get.js
+++ controllers/lists/get.js
@@ -4,7 +4,10 @@ const to = require('await-to-js').default;
 module.exports = async (req, res) => {
   const {storeId, skip, first} = req.query;
   const [findErr, findRes] = await to(GroceryList.find({storeId})
-  .populate('user').populate('items').sort({createdAt: 'asc'}).exec());
+  .populate('user').populate({
+    path: 'items',
+    populate: { path: 'item' }
+  }).sort({createdAt: 'asc'}).exec());
   if (findErr) return res.status(400).json(findErr);
   return res.json(findRes);
 };
