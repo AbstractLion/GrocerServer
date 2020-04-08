dfb466da338ac33139c9ab56468082bd81928fbf
Leon Si <leonzalion@gmail.com>
2020-04-08 10:31:27 -0400

    made changes

--- controllers/lists/complete.js
+++ controllers/lists/complete.js
@@ -6,8 +6,11 @@ let expo = new Expo();
 
 module.exports = async(req, res) => {
     const {qrCode} = req.body;
+    console.log(qrCode);
     const [findErr, findRes] = await to(GroceryList.findOne({qrCode}).populate('user').populate('store').exec());
+    console.log(findRes);
     if (findErr) return res.status(400).json(findErr);
+
     let user = findRes.user;
     let store = findRes.store;
     let pushToken = user.pushToken;
